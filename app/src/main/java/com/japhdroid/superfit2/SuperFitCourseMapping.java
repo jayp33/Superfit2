1ca12a3203b8abb44595a4c42e42abe469fabea0
JP Hashagen <jayp33e@gmail.com>
2015-12-05 12:09:04 +0100

    Map course file names to course names

---
+++ app/src/main/java/com/japhdroid/superfit2/SuperFitCourseMapping.java
@@ -0,0 +1,26 @@
+package com.japhdroid.superfit2;
+
+/**
+ * Created by User on 05.12.2015.
+ */
+public class SuperFitCourseMapping {
+
+    String[] type = {"Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs",
+            "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs",
+            "Team", "Team", "Team", "Team", "Team", "Team", "Team", "Team", "Team", "Team"};
+
+    // ruecken.jpg appears twice
+    String[] filename = {"bbp.jpg", "pilates.jpg", "bodyvive.jpg", "bodypumpxp.jpg",
+            "bodyattack.jpg", "bodystep.jpg", "bodypump.jpg", "bodybalance.jpg", "shbam.jpg",
+            "bauchxp.jpg", "zumba.jpg", "gritplyo.jpg", "bodycombat.jpg", "yoga.jpg", "lmistep.jpg",
+            "ruecken.jpg", "trx.jpg", "circuit.jpg", "tsm.jpg", "bauch.jpg", "cardio.jpg",
+            "trxbauch.jpg", "stretch.jpg", "ruecken.jpg", "po.jpg", "fullbodyworkout.jpg"};
+
+    String[] name = {"SuperFit Bauch Beine Po", "SuperFit Pilates", "LesMills BodyVive",
+            "LesMills BodyPump Express", "LesMills BodyAttack", "LesMills BodyStep",
+            "LesMills BodyPump", "LesMills BodyBalance", "LesMills Sh'Bam", "SuperFit Bauch Express",
+            "Zumba Fitness", "LesMills Grit Plyo", "LesMills BodyCombat", "SuperFit Yoga",
+            "LesMills LmiStep", "SuperFit Rücken", "TRX", "SuperFit Circuit",
+            "SuperFit Trainingsstart", "SuperFit Bauch", "SuperFit Cardio", "TRX Bauch",
+            "SuperFit Stretch", "SuperFit Rücken", "SuperFit Po", "SuperFit Full Body Workout"};
+}
