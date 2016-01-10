61857f28a611509463b324637645b89b87ad73ef
JP Hashagen <jayp33e@gmail.com>
2016-01-10 02:22:55 +0100

    Get Course Name from type and filename

--- app/src/main/java/com/japhdroid/superfit2/SuperFitCourseMapping.java
+++ app/src/main/java/com/japhdroid/superfit2/SuperFitCourseMapping.java
@@ -1,26 +1,45 @@
 package com.japhdroid.superfit2;
 
+import java.util.ArrayList;
+
 /**
  * Created by User on 05.12.2015.
  */
 public class SuperFitCourseMapping {
 
-    String[] type = {"Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs",
-            "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs",
+    static final String[] type = {"Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs",
+            "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs", "Kurs",
             "Team", "Team", "Team", "Team", "Team", "Team", "Team", "Team", "Team", "Team"};
 
     // ruecken.jpg appears twice
-    String[] filename = {"bbp.jpg", "pilates.jpg", "bodyvive.jpg", "bodypumpxp.jpg",
+    static final String[] filename = {"bbp.jpg", "pilates.jpg", "bodyvive.jpg", "bodypumpxp.jpg",
             "bodyattack.jpg", "bodystep.jpg", "bodypump.jpg", "bodybalance.jpg", "shbam.jpg",
             "bauchxp.jpg", "zumba.jpg", "gritplyo.jpg", "bodycombat.jpg", "yoga.jpg", "lmistep.jpg",
             "ruecken.jpg", "trx.jpg", "circuit.jpg", "tsm.jpg", "bauch.jpg", "cardio.jpg",
             "trxbauch.jpg", "stretch.jpg", "ruecken.jpg", "po.jpg", "fullbodyworkout.jpg"};
 
-    String[] name = {"SuperFit Bauch Beine Po", "SuperFit Pilates", "LesMills BodyVive",
+    static final String[] name = {"SuperFit Bauch Beine Po", "SuperFit Pilates", "LesMills BodyVive",
             "LesMills BodyPump Express", "LesMills BodyAttack", "LesMills BodyStep",
             "LesMills BodyPump", "LesMills BodyBalance", "LesMills Sh'Bam", "SuperFit Bauch Express",
             "Zumba Fitness", "LesMills Grit Plyo", "LesMills BodyCombat", "SuperFit Yoga",
             "LesMills LmiStep", "SuperFit Rücken", "TRX", "SuperFit Circuit",
             "SuperFit Trainingsstart", "SuperFit Bauch", "SuperFit Cardio", "TRX Bauch",
             "SuperFit Stretch", "SuperFit Rücken", "SuperFit Po", "SuperFit Full Body Workout"};
+
+    public static String getName(String _type, String _filename) {
+        ArrayList<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < filename.length; i++) {
+            if (filename[i].equalsIgnoreCase(_filename))
+                indices.add(i);
+        }
+        if (indices.size() > 1)
+            for (Integer i : indices) {
+                if (_type.equalsIgnoreCase(type[i]))
+                    return name[i];
+            }
+        if (indices.size() == 1)
+            return name[indices.get(0)];
+
+        return _type + " " + _filename;
+    }
 }
