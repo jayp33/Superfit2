80290be776a8fe4d6570388fd77ff555fb055e38
JP Hashagen <jayp33e@gmail.com>
2016-01-14 22:05:42 +0100

    Add sample RecyclerView with demo data from tutorial
    http://www.vogella.com/tutorials/AndroidRecyclerView/article.html

--- app/src/main/java/com/japhdroid/superfit2/MainActivity.java
+++ app/src/main/java/com/japhdroid/superfit2/MainActivity.java
@@ -1,13 +1,40 @@
 package com.japhdroid.superfit2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        ArrayList<String> myDataset = new ArrayList<>();
+        myDataset.add("Test 1");
+        myDataset.add("Test 2");
+        myDataset.add("Test 3");
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
