6390c4e2ad6802fd0d18a737e2736a2a808553cd
JP Hashagen <jayp33e@gmail.com>
2016-03-17 20:48:31 +0100

    Correct lesson count in tests

--- app/src/test/java/com/japhdroid/superfit2/LessonsTest.java
+++ app/src/test/java/com/japhdroid/superfit2/LessonsTest.java
@@ -26,7 +26,17 @@ public class LessonsTest {
     @Test
     public void testLessonCount() throws Exception {
         List<Lesson> lessons = new Lessons(new String[]{TestDataProvider.lessons_studio_3}, studios, courses).getLessons();
-        assertEquals(151, lessons.size());
+        int courseCount = 0;
+        int teamCount = 0;
+        for (Lesson lesson : lessons) {
+            if (lesson.getCourse().getFloor() == Course.Floor.KURS)
+                courseCount++;
+            else
+                teamCount++;
+        }
+        assertEquals(148, lessons.size());
+        assertEquals(69, courseCount); // 72 total, 3 conflicts
+        assertEquals(79, teamCount);   // 79 total, 0 conflicts
     }
 
     @Test
@@ -48,13 +58,13 @@ public class LessonsTest {
     public void testGetLessonsForSingleStudio() throws Exception {
         Lessons lessons = new Lessons(new String[]{TestDataProvider.lessons_studio_3,
                 TestDataProvider.lessons_studio_5}, studios, courses);
-        assertEquals(306, lessons.getLessons().size());
+        assertEquals(302, lessons.getLessons().size());
         List<Lesson> _lessons = lessons.getLessons(new Studio[]{studios.getStudioById(3)});
-        assertEquals(151, _lessons.size());
+        assertEquals(148, _lessons.size());
         for (Lesson lesson : _lessons)
             assertEquals(3, lesson.getStudio().getId());
         _lessons = lessons.getLessons(new Studio[]{studios.getStudioById(5)});
-        assertEquals(155, _lessons.size());
+        assertEquals(154, _lessons.size());
         for (Lesson lesson : _lessons)
             assertEquals(5, lesson.getStudio().getId());
     }
@@ -63,15 +73,15 @@ public class LessonsTest {
     public void testGetLessonsForMultipleStudios() throws Exception {
         Lessons lessons = new Lessons(new String[]{TestDataProvider.lessons_studio_3,
                 TestDataProvider.lessons_studio_5}, studios, courses);
-        assertEquals(306, lessons.getLessons().size());
+        assertEquals(302, lessons.getLessons().size());
         List<Lesson> _lessons = lessons.getLessons(new Studio[]{studios.getStudioById(3), studios.getStudioById(5)});
-        assertEquals(306, _lessons.size());
+        assertEquals(302, _lessons.size());
     }
 
     @Test
     public void testGetFutureLessonsForSingleStudio() throws Exception {
         Lessons lessons = new Lessons(new String[]{TestDataProvider.lessons_studio_3}, studios, courses);
-        assertEquals(136, lessons.getLessons().size());
+        assertEquals(148, lessons.getLessons().size());
         List<Lesson> _lessons = lessons.getLessons(new Studio[]{studios.getStudioById(3)}, true);
         for (int i = 0; i < _lessons.size() - 1; i++) {
             boolean actual = false;
